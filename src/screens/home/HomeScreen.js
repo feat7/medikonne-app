8a74d5e154648ee6301104b428ddc79b027fe7ab
feat7 <vinaykhobragade99@gmail.com>
2017-10-28 14:25:53 +0530

    Drawer Nagvigation Checkpoint

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { View, StatusBar } from "react-native";
 import { Container, Header, Title, Left, Icon, Right, Button, Body, Content,Text, Card, CardItem } from "native-base";
 export default class HomeScreen extends React.Component {
   render() {
@@ -22,20 +22,22 @@ export default class HomeScreen extends React.Component {
           <Card>
             <CardItem>
               <Body>
-                <Text>Chat App to talk some awesome people!</Text>
+                <Text>Health is Wealth and Medikonne is best!</Text>
               </Body>
             </CardItem>
           </Card>
+          <Container style={ { alignItems: 'center', justifyContent: 'center'} }>
           <Button full rounded dark
             style={{ marginTop: 10 }}
-            onPress={() => this.props.navigation.navigate("Chat")}>
-            <Text>Chat With People</Text>
+            onPress={() => this.props.navigation.navigate("Login")}>
+            <Text>Sign In</Text>
           </Button>
           <Button full rounded primary
             style={{ marginTop: 10 }}
-            onPress={() => this.props.navigation.navigate("Profile")}>
-            <Text>Goto Profiles</Text>
+            onPress={() => this.props.navigation.navigate("Register")}>
+            <Text>Sign Up</Text>
           </Button>
+          </Container>
         </Content>
       </Container>
     );
