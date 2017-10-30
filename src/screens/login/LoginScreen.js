a7ed51e84128c15e975bf5102ab97b867fb813a0
feat7 <vinaykhobragade99@gmail.com>
2017-10-31 04:04:11 +0530

    Login Registration implemented with server

--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import { Container, Header, Left, Body, Right, Button, Icon, Title, 
 Input, Item, Content, View, Text} from 'native-base';
+import { ToastAndroid } from 'react-native';
+import FormData from 'FormData';
+
 export default class LoginScreen extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      mobile: '',
+      password: '',
+    }
+  }
+
   render() {
     return (
       <Container>
@@ -25,12 +37,39 @@ export default class LoginScreen extends Component {
         backgroundColor: '#fff'}}>
         <Text style={{ padding: 4, marginBottom: 28}}>Login to Medikonne</Text>
           <Item rounded style={{ marginBottom: 4}}>
-            <Input placeholder="Mobile number" />
+            <Input placeholder="Mobile number"
+            onChangeText={(mobile) => this.setState({mobile: mobile })}
+            value={this.state.mobile} />
           </Item>
           <Item rounded style={{ marginBottom: 4}}>
-            <Input placeholder="Password" />
+            <Input secureTextEntry={true} placeholder="Password" 
+            onChangeText={(password) => this.setState({password: password })}
+            value={this.state.password}/>
           </Item>
-            <Button full rounded info bordered style={{ marginBottom: 28}}><Text>Login</Text></Button>
+            <Button full rounded info bordered style={{ marginBottom: 28}}
+            onPress={() =>   
+              {
+                var formdata = new FormData();
+  
+                console.log(this.state);
+                formdata.append('mobile', this.state.mobile)
+                formdata.append('password', this.state.password)
+              
+                fetch("http://10.0.2.2/app.login", {method: "POST", body: formdata})
+                      .then((response) => response.json())
+                      .then((responseData) => {
+                          this.setState({response: responseData});
+                          console.log(responseData)
+                          if(responseData.success) {
+                            ToastAndroid.showWithGravityAndOffset('Logged in Successfully!', ToastAndroid.LONG, ToastAndroid.BOTTOM, 25, 50);
+                          } else {
+                            ToastAndroid.showWithGravityAndOffset('Invaild Credentials', ToastAndroid.LONG, ToastAndroid.BOTTOM, 25, 50);                            
+                          }
+                      })
+                      .done()
+                  }    
+              
+            }><Text>Login</Text></Button>
         </View>
       </Container>
     );
--- src/screens/register/RegisterScreen.js
+++ src/screens/register/RegisterScreen.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
-import {KeyboardAvoidingView} from 'react-native';
 import { Container, Header, Left, Body, Right, Button, Icon, Title, 
 Input, Item, Content, View, Text} from 'native-base';
+import FormData from 'FormData';
+import { ToastAndroid } from 'react-native';
+
+
 export default class RegisterScreen extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      mobile: '',
+      password: '',
+      confirm_password: '',
+    }
+  }
+
   render() {
     return (
       <Container>
@@ -26,18 +40,49 @@ export default class RegisterScreen extends Component {
         backgroundColor: '#fff'}}>
         <Text style={{ padding: 4, marginBottom: 28}}>Register to Medikonne</Text>
           <Item rounded style={{ marginBottom: 4}}>
-            <Input placeholder="Full Name" />
+            <Input placeholder="Full Name" 
+            onChangeText={(name) => this.setState({name: name })}
+            value={this.state.name}/>
           </Item>
           <Item rounded style={{ marginBottom: 4}}>
-            <Input placeholder="Mobile Number" />
+            <Input placeholder="Mobile Number" 
+            onChangeText={(mobile) => this.setState({mobile: mobile })}
+            value={this.state.mobile}/>
           </Item>
           <Item rounded style={{ marginBottom: 4}}>
-            <Input secureTextEntry={true} placeholder="Password" />
+            <Input secureTextEntry={true} placeholder="Password"
+            onChangeText={(password) => this.setState({password: password })} 
+            value={this.state.password}/>
           </Item>
           <Item rounded style={{ marginBottom: 4}}>
-            <Input secureTextEntry={true} placeholder="Confirm Password" />
+            <Input secureTextEntry={true} placeholder="Confirm Password"
+            onChangeText={(confirm_password) => this.setState({confirm_password: confirm_password })}
+            value={this.state.confirm_password} />
           </Item>
-          <Button full rounded info style={{ marginBottom: 38}}><Text>Sign Up</Text></Button>
+          <Button full rounded info style={{ marginBottom: 38}}
+          onPress={() =>   
+            {
+              var formdata = new FormData();
+
+              console.log(this.state);
+              formdata.append('name', this.state.name)
+              formdata.append('mobile', this.state.mobile)
+              formdata.append('password', this.state.password)
+              formdata.append('confirm_password', this.state.confirm_password)
+            
+              fetch("http://10.0.2.2/app.register", {method: "POST", body: formdata})
+                    .then((response) => response.json())
+                    .then((responseData) => {
+                        this.setState({response: responseData});
+                        console.log(responseData)
+                        if(responseData.success) {
+                          ToastAndroid.showWithGravityAndOffset('Registered Successfully!', ToastAndroid.LONG, ToastAndroid.BOTTOM, 25, 50);
+                        }
+                    })
+                    .done()
+                }    
+            
+          }><Text>Sign Up</Text></Button>
         </View>
       </Container>
     );
